1891d56c767f8991101c8508e23795a321394900
Javier Gonzalez nicora <javgonza@c3r2s2.42madrid.com>
2022-03-17 19:41:39 +0100

    Provided a test for generate_question

---
+++ tests/generate_questions/generate_question_only_yes_answer.sh
@@ -0,0 +1,18 @@
+FOLDER_ROOT=$(dirname $0)
+
+./$FOLDER_ROOT/../../source/generate_question.sh "QUESTION?" "$FOLDER_ROOT/script_yes" > "$FOLDER_ROOT/.question"
+
+chmod a+x "$FOLDER_ROOT/.question"
+
+ANSWER=$(echo y | "$FOLDER_ROOT/.question")
+echo Test result: "$ANSWER"
+
+
+#This ugly thing is kind of a bug. A solution would involve deviating the echo of question so that we only get the line containing the answer
+if [ "$ANSWER" == "
+affirmative" ]
+then
+	echo TEST_OK
+else
+	echo TEST_NO_OK
+fi
