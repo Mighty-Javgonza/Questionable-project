a236b40313602d96b416c1ec5e785b31ef104a91
Javier Gonzalez nicora <javgonza@student.42madrid.com>
2022-04-12 10:28:58 +0200

    Corrected bug on tests

--- tests/generate_questions/generate_question_yes_and_no.sh
+++ tests/generate_questions/generate_question_yes_and_no.sh
@@ -4,13 +4,11 @@ FOLDER_ROOT=$(dirname $0)
 
 chmod a+x "$FOLDER_ROOT/.question"
 
-ANSWER=$(echo y | "$FOLDER_ROOT/.question")
+ANSWER=$(echo y | "$FOLDER_ROOT/.question" | grep -v ^$)
 echo Test result: "$ANSWER"
 
 
-#This ugly thing is kind of a bug. A solution would involve deviating the echo of question so that we only get the line containing the answer
-if [ "$ANSWER" == "
-affirmative" ]
+if [ "$ANSWER" == "affirmative" ]
 then
 	echo TEST_OK
 else
